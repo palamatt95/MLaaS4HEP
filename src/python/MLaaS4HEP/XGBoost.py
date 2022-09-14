c45082c01f8e0908c30e1f829b08154b624a118e
MPal <palamatt95@gmail.com>
2022-09-14 17:22:21 +0200

    adjusting models parameters

--- src/python/MLaaS4HEP/XGBoost.py
+++ src/python/MLaaS4HEP/XGBoost.py
@@ -3,11 +3,11 @@ import xgboost as xgb
 def model():
     params = {
         'objective':'binary:logistic',
-        'max_depth': 9,
+        'max_depth': 7,
         'subsample': 0.9,
         'alpha': 10,
         'learning_rate': 0.0001,
-        'n_estimators': 3000
+        'n_estimators': 1500
     }
     xgb_model = xgb.XGBClassifier(**params)
     return xgb_model
